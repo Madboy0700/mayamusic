8bb0e4909ad58c199eb36b23fff43075fe27be8a
Madboy0700 <bobimobi07@gmail.com>
2024-12-16 18:36:41 +0300

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO", #ticaret
-    "https://t.me/AcelyaTicaret", 
+    "https://t.me/Etikettaggerbot", 
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
